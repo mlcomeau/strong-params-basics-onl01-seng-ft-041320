f73a35a62a39f84b946b4f6acc3e7b8869f57009
Megan Comeau <mlcomeau99@gmail.com>
2020-06-12 22:20:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,18 +16,18 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def edit
 	  @post = Post.find(params[:id])
 	end
 
-	private 
+	private
 
 	def post_params(*args)
 	  params.require(:post).permit(*args)
